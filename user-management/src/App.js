d9732921ed534ee46470672b9347ef25363d0e43
Clinton Dmello <clinton.dmello95@gmail.com>
2021-04-21 00:43:46 +0530

    User Management Portal

---
+++ user-management/src/App.js
@@ -0,0 +1,32 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+// import NavBar from "./components/layout/Navbar";
+import NavBar from "./components/layout/Navbar.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import NotFound from "./components/pages/NotFound";
+import AddUser from "./components/users/AddUser";
+import EditUser from "./components/users/EditUser";
+import User from "./components/users/User";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+          <Route exact path="/user/add" component={AddUser}></Route>
+          <Route exact path="/user/edit/:id" component={EditUser}></Route>
+          <Route exact path="/user/:id" component={User}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
